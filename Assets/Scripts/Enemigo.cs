1f2702f64f73cfe389234149f31f22daf8d4dfa8
enriqueguz1701 <enriqueguz1701@gmail.com>
2026-04-28 14:31:42 +0200

    Solución campo de visión del enemigo

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -12,7 +12,7 @@ public class Enemigo : MonoBehaviour
     [SerializeField] bool persiguiendoJugador;
 
     [SerializeField] RaycastHit raycast;
-    [SerializeField] float campoVision;
+    [SerializeField] float campoVision, anguloVision;
     [SerializeField] bool atacando;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,45 +28,60 @@ public class Enemigo : MonoBehaviour
     void Update()
     {
 
-        if (!persiguiendoJugador && agent.remainingDistance <= agent.stoppingDistance)
+        if (PuedeVerJugador())
         {
-            puntoActual++;
-            if(puntoActual == puntos.Length)
-            {
-                puntoActual = 0;
-            }
-            agent.SetDestination(puntos[puntoActual].position);
-        }
-        
-
-        Debug.DrawRay(transform.position, transform.forward, Color.red, 5);
-        if (Physics.Raycast(transform.position, transform.forward, out raycast, campoVision))
-        {
-            if (raycast.transform.tag == "Player")
-            {
-                Debug.Log("He visto al jugador");
-                persiguiendoJugador = true;
-                agent.SetDestination(jugador.position); 
-                if (!atacando)
-                {
-                    StartCoroutine(Atacar());
-                }
-            }
-            else
-            {
-                persiguiendoJugador = false;
-                atacando = false;
-                agent.SetDestination(puntos[puntoActual].position);
-                StopAllCoroutines();
-            }
+            persiguiendoJugador = true;
+            agent.SetDestination(jugador.position);
         }
         else
         {
             persiguiendoJugador = false;
-            atacando = false;
-            agent.SetDestination(puntos[puntoActual].position);
-            StopAllCoroutines();
+            if(agent.remainingDistance <= agent.stoppingDistance)
+            {
+                puntoActual++;
+                if (puntoActual == puntos.Length)
+                {
+                    puntoActual = 0;
+                }
+                agent.SetDestination(puntos[puntoActual].position);
+            }
         }
+    }
+
+    public bool PuedeVerJugador()
+    {
+        //Calculo la dirección en la que está el jugador restando su posición
+        //menos la del enemigo
+        Vector3 direccionJugador = jugador.position - transform.position;
+        //Como dirección es un vector y necesito un número tengo que calcular la 
+        //magnitud de ese vector (raíz cuadrada de x^2 + y^2 + z^2)
+        float distancia = direccionJugador.magnitude;
+
+        //Si esa distancia es más grande que el campo de visión del enemigo quiere
+        //decir que no lo está viendo
+        if (distancia > campoVision)
+            return false;
+
+        //Ahora calculo el ángulo que hay entre el enemigo y el jugador
+        float angulo = Vector3.Angle(transform.forward, direccionJugador);
+
+        //Si ese ángulo es más grande que el ángulo de visión del enemigo
+        //quiere decir que no lo está viendo
+        if (angulo > anguloVision / 2f)
+            return false;
+
+        Debug.DrawRay(transform.position, direccionJugador.normalized, Color.red, 5);
+        //En esta parte el jugador ya está en el campo de visión del enemigo, pero puede haber obstáculos
+        //que impiden que el enemigo vea al jugador
+        //Para comprobar si hay obstáculos disparamos un rayo desde el enemigo hacia el jugador
+        if (Physics.Raycast(transform.position, direccionJugador.normalized, out RaycastHit hit, distancia))
+        {
+            //Si el objeto con el que impacta el rayo es el jugador devolvemos verdadero
+            return hit.transform.tag == "Player";
+        }
+
+        //Si el objeto con el que impacta el rayo no es el jugador devolvemos falso
+        return false;
     }
 
     IEnumerator Atacar()
